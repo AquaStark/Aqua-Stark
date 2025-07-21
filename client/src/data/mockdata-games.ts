0d21e54410eaa8385d499ac332dcfb22616e4f0d
Josue19-08 <josuemarin2009@hotmail.com>
2025-07-21 12:45:31 -0600

    feat: add mock data for fish and mini-games

---
+++ client/src/data/mockdata-games.ts
@@ -0,0 +1,56 @@
+export const mockFish = [
+    {
+      id: "fish001",
+      name: "Aqua Puffer",
+      image: "/fish/fish1.png",
+    },
+    {
+      id: "fish002",
+      name: "Crimson Koi",
+      image: "/fish/fish2.png",
+    },
+    {
+      id: "fish003",
+      name: "Neon Tetra",
+      image: "/fish/fish3.png",
+    },
+    {
+      id: "fish004",
+      name: "Golden Scale",
+      image: "/fish/fish4.png",
+    },
+  ]
+  
+  export const mockGames = [
+    {
+      id: "game001",
+      name: "Floppy Fish",
+      image: "/habitat/freshwater.png",
+      link: "/game/floppy-fish",
+    },
+    {
+      id: "game002",
+      name: "Bubble Jumper",
+      image: "/habitat/saltwater.png",
+      link: "/game/bubble-jumper",
+    },
+    {
+      id: "game003",
+      name: "Echo Coral",
+      image: "/habitat/deepsea.png",
+      link: "/game/echo-coral",
+    },
+    {
+      id: "game004",
+      name: "Jetstream Runner",
+      image: "/habitat/saltwater.png",
+      link: "/game/jetstream-runner",
+    },
+    {
+      id: "game005",
+      name: "Reef Dash",
+      image: "/habitat/freshwater.png",
+      link: "/game/reef-dash",
+    },
+  ]
+  
