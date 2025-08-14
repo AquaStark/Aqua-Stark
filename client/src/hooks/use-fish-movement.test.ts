da5641e2faa912893431f9435731bc0455e0ece0
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-08-14 23:55:36 +0100

    feat: fixed errors

--- client/src/hooks/use-fish-movement.test.ts
+++ client/src/hooks/use-fish-movement.test.ts
@@ -76,7 +76,7 @@ describe('useFishMovement', () => {
       useFishMovement(mockFish, { aquariumBounds: mockAquariumBounds })
     );
 
-    // const initialPosition = { ...result.current[0].position };
+    const initialPosition = { ...result.current[0].position };
 
     // Simulate animation frame calls
     act(() => {
