394269992006135e49cf1c10a44c91d899ed3616
Josue19-08 <josuemarin2009@hotmail.com>
2025-08-14 22:46:10 -0600

    config: update vitest configuration

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -12,7 +12,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
     css: true,
     exclude: [
       '**/node_modules/**',
