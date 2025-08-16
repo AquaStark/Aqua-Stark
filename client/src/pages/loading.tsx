63ac105d34669f37439757132152adb99ef23a35
Fran19-09 <francinnim8@gmail.com>
2025-08-16 16:23:13 -0600

    feat: add loading page

---
+++ client/src/pages/loading.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useNavigate } from 'react-router-dom';
+import { LoadingScreen } from '@/components/loading/loading-screen';
+
+export default function LoadingPage() {
+  const navigate = useNavigate();
+
+  const handleComplete = () => {
+    navigate('/game');
+  };
+
+  return (
+    <LoadingScreen
+      onComplete={handleComplete}
+      duration={5000}
+      showTips={true}
+    />
+  );
+}
