943ad5ab0e14f22f9fd16b8df9240bf1a76a9688
Josue19-08 <josuemarin2009@hotmail.com>
2025-09-02 19:06:04 -0600

    feat(game-menu): apply TypeScript typings to component props

--- client/src/components/game/game-menu.tsx
+++ client/src/components/game/game-menu.tsx
@@ -17,42 +17,65 @@ interface GameMenuProps {
 }
 
 export function GameMenu({ show }: GameMenuProps) {
-
   const menuItems = [
-    { icon: <Volume2 className='h-5 w-5' />, onClick: () => {}, tooltip: 'Volume' },
-    { icon: <Trophy className='h-5 w-5' />, onClick: () => {}, tooltip: 'Achievements' },
-    { icon: <ShoppingBag className='h-5 w-5' />, onClick: () => {}, tooltip: 'Shop' },
-    { icon: <HelpCircle className='h-5 w-5' />, onClick: () => {}, tooltip: 'Help' },
-    { icon: <Camera className='h-5 w-5' />, onClick: () => {}, tooltip: 'Screenshot' },
+    {
+      icon: <Volume2 className='h-5 w-5' />,
+      onClick: () => {},
+      tooltip: 'Volume',
+    },
+    {
+      icon: <Trophy className='h-5 w-5' />,
+      onClick: () => {},
+      tooltip: 'Achievements',
+    },
+    {
+      icon: <ShoppingBag className='h-5 w-5' />,
+      onClick: () => {},
+      tooltip: 'Shop',
+    },
+    {
+      icon: <HelpCircle className='h-5 w-5' />,
+      onClick: () => {},
+      tooltip: 'Help',
+    },
+    {
+      icon: <Camera className='h-5 w-5' />,
+      onClick: () => {},
+      tooltip: 'Screenshot',
+    },
     { icon: <Home className='h-5 w-5' />, onClick: () => {}, tooltip: 'Home' },
-    { icon: <Settings className='h-5 w-5' />, onClick: () => {}, tooltip: 'Settings' },
+    {
+      icon: <Settings className='h-5 w-5' />,
+      onClick: () => {},
+      tooltip: 'Settings',
+    },
     // Debug buttons
-    { 
-      icon: <Fish className='h-5 w-5' />, 
+    {
+      icon: <Fish className='h-5 w-5' />,
       onClick: () => {
         const feedingDebug = document.querySelector('[data-feeding-debug]');
         if (feedingDebug) {
           feedingDebug.classList.toggle('hidden');
         }
       },
-      tooltip: 'Feeding Debug'
+      tooltip: 'Feeding Debug',
     },
-    { 
-      icon: <Sparkles className='h-5 w-5' />, 
+    {
+      icon: <Sparkles className='h-5 w-5' />,
       onClick: () => {
         const dirtDebug = document.querySelector('[data-dirt-debug]');
         if (dirtDebug) {
           dirtDebug.classList.toggle('hidden');
         }
       },
-      tooltip: 'Dirt Debug'
+      tooltip: 'Dirt Debug',
     },
   ];
 
   return (
     <AnimatePresence>
       {show && (
-        <motion.div 
+        <motion.div
           className='absolute top-28 right-0 z-50 flex flex-col gap-2 transform -translate-x-[100%]'
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -65,10 +88,10 @@ export function GameMenu({ show }: GameMenuProps) {
               initial={{ opacity: 0, scale: 0.8, y: -20 }}
               animate={{ opacity: 1, scale: 1, y: 0 }}
               exit={{ opacity: 0, scale: 0.8, y: -20 }}
-              transition={{ 
-                duration: 0.3, 
+              transition={{
+                duration: 0.3,
                 delay: index * 0.1,
-                ease: "easeOut"
+                ease: 'easeOut',
               }}
             >
               <GameButton
