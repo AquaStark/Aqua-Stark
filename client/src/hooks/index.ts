6df1659b68b575b32769ca447376e73531762a36
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-09-12 17:36:36 +0100

    chore:  add named export to hooks index file for all hooks including nested hooks and categorized them with comments

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,1 +1,31 @@
+export { useAquarium } from './use-aquarium';
+export { useBubbles } from './use-bubbles';
+export { useCartridgeConnection } from './use-cartridge-connection';
+export { useCartridgeSession } from './use-cartridge-session';
+export { useCommunity } from './use-community';
+export { useStarknetConnect } from './use-connector';
+export { useDebounce } from './use-debounce';
+export { useDevConsoleHandlers } from './use-dev-console-handlers';
+export { useDirtSystemFixed } from './use-dirt-system-fixed';
+export { useDirtSystem } from './use-dirt-system';
+export { useEncyclopedia } from './use-encyclopedia';
+export { useEventsCalendar } from './use-events-calendar';
+export { useExperience } from './use-experience';
+export { useFishMovement } from './use-fish-movement';
+export { useFishStats } from './use-fish-stats';
+export { useFoodSystem } from './use-food-system';
+export { useGames } from './use-games';
+export { useHelpCenter } from './use-help-center';
 export { useLoadingNavigation } from './use-loading-navigation';
+export { useSettings } from './use-settings';
+export { useSimpleWalletConnection } from './use-simple-wallet-connection';
+export { useStoreFilters } from './use-store-filters';
+export { useFishIndicators } from './useFishIndicators';
+export { useGameActions } from './useGameActions';
+export { usePlayerValidation } from './usePlayerValidation';
+
+// dojo hooks
+export * from './dojo/index';
+
+// minigames hook (categories nested within)
+export * from './minigames/floppy-fish/index';
